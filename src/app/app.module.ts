bf953ec3d7624e272f357e550081531906a61a39
NikhilElavarasu <rockingnikhil8@gmail.com>
2020-06-29 16:31:47 +0530

    assignment changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { MatListModule } from "@angular/material/list";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 import { AppComponent } from './app.component';
 import "hammerjs";
@@ -24,6 +26,8 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatToolbarModule,
     FlexLayoutModule,
     MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
