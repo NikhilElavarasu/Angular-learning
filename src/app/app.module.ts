0d2fa2111e36f72a0e0dee2291e8495631090c3e
NikhilElavarasu <rockingnikhil8@gmail.com>
2020-07-05 10:38:29 +0530

    single page application part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 
 import { AppComponent } from './app.component';
-import "hammerjs";
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
@@ -19,7 +18,9 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from "./services/dish.service";
+import { PromotionService } from "./services/promotion.service";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import "hammerjs";
 
 @NgModule({
   declarations: [
@@ -45,6 +46,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
   ],
   providers: [
     DishService,
+    PromotionService,
   ],
   bootstrap: [AppComponent]
 })
